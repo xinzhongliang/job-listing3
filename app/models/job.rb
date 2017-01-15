6fbe8717efa9955c8b3d82c6e3d72869eda82134
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-01-15 21:53:52 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,12 +1,14 @@
 class Job < ApplicationRecord
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
   def publish!
     self.is_hidden = false
-    self.save
+    save
   end
 
   def hide!
     self.is_hidden = true
-    self.save
+    save
   end
   validates :title, presence: true
   validates :wage_lower_bound, presence: true
