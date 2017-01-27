71015809d168cad1fc246c7580cc461175be0858
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-01-27 08:51:03 +0800

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts '這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs'
+puts '這個種子檔會自動建立一個admin帳號 '
 
 create_account = User.create([email: 'jol@123.com', password: '123123', password_confirmation: '123123', is_admin: 'true'])
 puts 'Admin account created.'
-
-create_jos = for i in 1..5 do
-               Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99) * 100, wage_lower_bound: rand(10..49) * 100, is_hidden: 'false'])
-end
-puts '10 Public jobs created.'
-
-create_jos = for i in 1..5 do
-               Job.create!([title: "Job no.#{i + 5}", description: "這是用種子建立的第 #{i + 10} 個Hidden工作", wage_upper_bound: rand(50..99) * 100, wage_lower_bound: rand(10..49) * 100, is_hidden: 'true'])
-end
-puts '10 Hidden jobs created.'
